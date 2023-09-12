a3d71ca2343f16ee9effddb84a4033cc7b41e6f5
egoist-hozumi <taowen.qian@mail.utoronto.ca>
2023-09-11 22:44:10 -0400

    Reduce

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+
+        int count = 0, i = 100;
+        while (i != 0) {
+            if (i % 2 == 0) {
+                i = i / 2;
+            }
+            else {
+                i = i - 1;
+            }
+            count += 1;
+        }
+        System.out.println(count);
+    }
 }
